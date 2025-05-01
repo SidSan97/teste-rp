7dd491ca4d9f1fb34961691786f3411793a25c85
SidSan97 <sidnei1.8santiago@hotmail.com>
2025-05-01 19:25:42 -0300

    feature: Criando migrations para tabela de produtos

---
+++ database/migrations/2025_05_01_151535_create_products.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id('id_products');
+            $table->string('name');
+            $table->text('description');
+            $table->integer('quantity');
+            $table->float('price', 5, 2);
+            $table->string('category');
+            $table->string('sku')->unique();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('products');
+    }
+};
