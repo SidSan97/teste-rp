f92a42baf93488abde23aaf26e64471b29a2630d
SidSan97 <sidnei1.8santiago@hotmail.com>
2025-05-01 19:24:22 -0300

    feature: Adicionando rota para API

---
+++ routes/api.php
@@ -0,0 +1,14 @@
+<?php
+
+use App\Http\Controllers\Api\ProductsController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+
+Route::middleware('auth:api')->group(function () {
+    Route::get('v1/produtos', [ProductsController::class, 'index']);
+});
