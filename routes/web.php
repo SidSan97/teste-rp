12240aba14fa6d497b0dfdee45f614a6562d214b
SidSan97 <sidnei1.8santiago@hotmail.com>
2025-05-01 21:34:10 -0300

    chore: Removendo rotas não necessárias

--- routes/web.php
+++ routes/web.php
@@ -12,9 +12,5 @@ Route::middleware(['auth', 'verified'])->group(function () {
     Route::get('dashboard', [ProductsController::class, 'index'])->name('dashboard');
 });
 
-Route::get('/login-api', function () {
-    return Inertia::render('auth/login-api');
-});
-
 require __DIR__.'/settings.php';
 require __DIR__.'/auth.php';
