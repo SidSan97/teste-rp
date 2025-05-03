6f941195f38275eeffe6ceeca3f84898c8cd330f
SidSan97 <sidnei1.8santiago@hotmail.com>
2025-05-03 19:20:29 -0300

    feature: Adicionando filtro

--- resources/js/components/product-table.tsx
+++ resources/js/components/product-table.tsx
@@ -7,6 +7,19 @@ export default function ProductTable({ products, userLevel }: any) {
     const [isOpen, setIsOpen] = useState(false);
     const [selectedProduct, setSelectedProduct] = useState(null);
 
+    const [searchName, setSearchName] = useState('');
+    const [searchCategory, setSearchCategory] = useState('');
+    const [minPrice, setMinPrice] = useState('');
+    const [maxPrice, setMaxPrice] = useState('');
+
+    const filteredProducts = products.filter((product: { name: string; category: string; price: string; }) => {
+        const matchName = product.name.toLowerCase().includes(searchName.toLowerCase());
+        const matchCategory = product.category.toLowerCase().includes(searchCategory.toLowerCase());
+        const matchMin = minPrice === '' || parseFloat(product.price) >= parseFloat(minPrice);
+        const matchMax = maxPrice === '' || parseFloat(product.price) <= parseFloat(maxPrice);
+        return matchName && matchCategory && matchMin && matchMax;
+    });
+
     const handleDelete = (id: Number) => {
         Swal.fire({
             title: "Deseja mesmo excluir o produto?",
@@ -39,6 +52,54 @@ export default function ProductTable({ products, userLevel }: any) {
 
     return (
         <>
+            <h6 className='mb-2'>Filtros:</h6>
+            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 mb-4">
+                <div className="flex flex-col">
+                    <label className="mb-1 text-sm font-medium text-gray-700">Buscar por nome</label>
+                    <input
+                        type="text"
+                        placeholder="Ex: Camiseta"
+                        className="border p-2 rounded"
+                        value={searchName}
+                        onChange={(e) => setSearchName(e.target.value)}
+                    />
+                </div>
+
+                <div className="flex flex-col">
+                    <label className="mb-1 text-sm font-medium text-gray-700">Buscar por categoria</label>
+                    <input
+                        type="text"
+                        placeholder="Ex: Roupas"
+                        className="border p-2 rounded"
+                        value={searchCategory}
+                        onChange={(e) => setSearchCategory(e.target.value)}
+                    />
+                </div>
+
+                <div className="flex flex-col">
+                    <label className="mb-1 text-sm font-medium text-gray-700">Preço mínimo</label>
+                    <input
+                        type="number"
+                        placeholder="0.00"
+                        className="border p-2 rounded"
+                        value={minPrice}
+                        onChange={(e) => setMinPrice(e.target.value)}
+                    />
+                </div>
+
+                <div className="flex flex-col">
+                    <label className="mb-1 text-sm font-medium text-gray-700">Preço máximo</label>
+                    <input
+                        type="number"
+                        placeholder="100.00"
+                        className="border p-2 rounded"
+                        value={maxPrice}
+                        onChange={(e) => setMaxPrice(e.target.value)}
+                    />
+                </div>
+            </div>
+
+
             <div className="w-full overflow-x-auto rounded-lg shadow">
                 <table className="min-w-[1000px] text-sm text-left text-gray-700">
                     <thead className="bg-gray-100 text-xs uppercase tracking-wider text-gray-700">
@@ -56,8 +117,8 @@ export default function ProductTable({ products, userLevel }: any) {
                         </tr>
                     </thead>
                     <tbody className="bg-white divide-y divide-gray-200">
-                        {products?.length > 0 ? (
-                            products.map((product, index) => (
+                        {filteredProducts?.length > 0 ? (
+                            filteredProducts.map((product, index) => (
                                 <tr key={product.id_products}>
                                     <td className="px-4 py-2">{index + 1}</td>
                                     <td className="px-4 py-2">{product.name}</td>
