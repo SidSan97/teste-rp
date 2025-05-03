35904678ed50792c5494e24e094c21b6e2bf4628
SidSan97 <sidnei1.8santiago@hotmail.com>
2025-05-03 20:14:49 -0300

    chore: Renomeando arquivo e classe

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,8 @@
 <?php
 
-use App\Http\Controllers\Api\ProductsController;
+use App\Http\Controllers\Api\ProductsControllerResponse;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:api')->group(function () {
-    Route::get('v1/produtos', [ProductsController::class, 'index']);
+    Route::get('v1/produtos', [ProductsControllerResponse::class, 'index']);
 });
